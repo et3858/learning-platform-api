ef631851c1adb72a5998f5bffc253c272aa710b1
ekindall <eduardo@rflex.cl>
2021-07-13 17:54:49 -0400

    feat(User): implementa modelo User usando conexión a mongoDB

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,25 +1,67 @@
-const UserModel = require("../models/user.model");
+// const UserModel = require("../models/user.model");
+const User = require("../models/user.model.db");
 
 const UserController = {
     index: ((req, res) => {
-        let users = UserModel.getUsers();
-        res.json(users);
+        User.find(function (err, users) {
+            if (err) res.send(500, err.message);
+            res.status(200).jsonp(users);
+        });
     }),
     store: ((req, res) => {
-        UserModel.addUser(req.body);
-        res.send("Aplicando store() en UserController");
+        var user = new User({
+            name: req.body.name,
+        });
+
+        user.save(function (err, user) {
+            if (err) return res.status(500).send(err.message);
+            res.status(200).jsonp(user);
+        });
     }),
     show: ((req, res) => {
-        let user = UserModel.getUserByID(req.params.id);
-        res.json(user);
+        User.findById(req.params.id, function (err, user) {
+            if (err) return res.status(500).send(err.message);
+
+            if (user) {
+                res.status(200).jsonp(user);
+            } else {
+                res.send();
+            }
+        });
     }),
     update: ((req, res) => {
-        UserModel.editUser(req.body, req.params.id);
-        res.send("Aplicando update() en UserController");
+        User.findById(req.params.id, function(err, user) {
+            if (err) return res.status(500).send(err.message);
+
+            // Stop the process if user doesn't exist
+            if (!user) {
+                res.send();
+                return;
+            }
+
+            user.name = req.body.name;
+
+            user.save(function (err) {
+                if (err) return res.status(500).send(err.message);
+                res.status(200).jsonp(user);
+            });
+        });
     }),
     destroy: ((req, res) => {
-        UserModel.deleteUser(req.params.id);
-        res.send("Aplicando destroy() en UserController");
+        User.findById(req.params.id, function (err, user) {
+            if (err) return res.status(500).send(err.message);
+
+            // Stop the process if user doesn't exist
+            if (!user) {
+                res.send();
+                return;
+            }
+
+            user.remove(function (err) {
+                if (err) return res.status(500).send(err.message);
+                res.status(200).send();
+            });
+        });
     })
 };
 
---
+++ models/user.model.db.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("User", UserSchema);
