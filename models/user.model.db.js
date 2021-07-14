8d680beba758eb3e85f21298c488a7b28b199fdc
ekindall <eduardo@rflex.cl>
2021-07-14 15:41:43 -0400

    feat(user.model): agrega el campo updated_at y crea un pre hook que actualiza la fecha de dicho campo

--- models/user.model.db.js
+++ models/user.model.db.js
@@ -10,7 +10,17 @@ var UserSchema = new Schema({
     created_at: {
         type: Date,
         default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
     }
+});
+
+UserSchema.pre("save", function (next) {
+    // Update to current datetime before saving
+    this.updated_at = new Date();
+    next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
