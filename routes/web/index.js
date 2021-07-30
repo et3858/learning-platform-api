5aa338a53eca07609ce73d7e86b833318ca925eb
ekindall <eduardo@rflex.cl>
2021-07-30 11:57:37 -0400

    feat(views): creación del template my-page

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,11 +1,24 @@
 var express = require("express");
-// var path = require('path');
 var router = express.Router();
+
+const Category = include("models/category.model");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render("index", { title: "Express" });
   res.render("index", { title: "Mi proyecto con Express" });
+});
+
+/* GET my-page. */
+router.get('/lol', function(req, res, next) {
+  Category
+    .find({})
+    .populate("courses")
+    .exec((err, categories) => {
+      if (err) return res.send(500, err.message);
+
+      res.render("my-page", { categories });
+    })
 });
 
 module.exports = router;
