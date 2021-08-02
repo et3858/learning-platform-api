ce1fcf405e5dbc0c01afb922daaa5033ccc53b43
ekindall <eduardo@rflex.cl>
2021-08-02 11:27:23 -0400

    feat(test): creando service db_handler

---
+++ test/db_handler.js
@@ -0,0 +1,39 @@
+// Help source: https://dev.to/paulasantamaria/testing-node-js-mongoose-with-an-in-memory-database-32np
+
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+let mongod;
+
+// Connect to DB
+module.exports.connect = async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+
+    const mongooseOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        poolSize: 10
+    };
+    await mongoose.connect(uri, mongooseOptions);
+};
+
+// Disconnect and close connection
+module.exports.closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+};
+
+// Clear the DB and remove all data
+module.exports.clearDatabase = async () => {
+    if (process.env.NODE_ENV !== "test") {
+        throw new Error("Attempt to clear non testing database!");
+    }
+
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany()
+    }
+};
