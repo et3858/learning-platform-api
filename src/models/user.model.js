b965fb0aa4084e7a382daee1ece1a74d90fb0ae1
ekindall <eduardo@rflex.cl>
2021-08-06 16:12:26 -0400

    feat(user.model): crea funciones que hashea y compara una contraseña creada o modificada

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -42,5 +43,37 @@ UserSchema.pre("findOneAndUpdate", function (next) {
     this._update.updated_at = new Date();
     next();
 });
+
+UserSchema.pre("save", function (next) {
+    let user = this;
+
+    // Only hash the password if it has been modified (or is new)
+    // Help source: https://stackoverflow.com/a/14595363
+    if (!user.isModified("password")) return next();
+    bcrypt
+        .hash(user.password, 10)
+        .then(hash => {
+            user.password = hash;
+            next();
+        })
+        .catch(err => {
+            console.log(`Error in hashing password: ${err.message}`);
+            next(err);
+        });
+});
+
+/**
+ * Returns a Promise function
+ * @param   string   inputPassword
+ * @returns function
+ */
+UserSchema.methods.passwordComparison = function(inputPassword, cb) {
+    let user = this;
+
+    bcrypt.compare(inputPassword, user.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model("User", UserSchema);
