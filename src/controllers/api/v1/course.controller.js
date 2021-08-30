7b2eb040989110aa78d34f9e399670b60f8ff57c
Eduardo Tejada <et3858@gmail.com>
2021-08-30 16:56:32 -0400

    feat(courses): crea sistema de paginación

--- src/controllers/api/v1/course.controller.js
+++ src/controllers/api/v1/course.controller.js
@@ -9,13 +9,9 @@ const CourseController = {
         // and isolating each of them ('populate_with', 'select_only', 'limit' and 'page') as independent variables
         const { populate_with, select_only, limit, page, ...query } = req.query;
 
-        Course
-            .find(query, select_only, pagination.getOptions(limit, page))
-            .populate(populate_with)
-            .exec((err, courses) => {
-                if (err) return res.status(500).send(err.message);
-                res.status(200).jsonp({ data: courses });
-            });
+        pagination.responder(Course.find(query, select_only, pagination.getOptions(limit, page)).populate(populate_with))
+            .then(data => res.status(200).jsonp(data))
+            .catch(err => res.status(500).send(err.message));
     }),
     getCourseBySlug: ((req, res) => {
         Course
--- src/services/pagination.js
+++ src/services/pagination.js
@@ -5,3 +5,67 @@
  * @returns object
  */
 exports.getOptions = (limit = 0, page = 1) => ({ limit, skip: (page - 1) * limit });
+
+
+
+/**
+ * Paginate the results
+ * @param   object modelQuery [Must be a instance of mongoose.Query]
+ * @returns object            [{ data, paginate }]
+ */
+exports.responder = (modelQuery) => {
+    return new Promise((resolve, reject) => {
+
+        modelQuery.exec((err, data) => {
+            if (err) return reject(err);
+
+            let limit = modelQuery.options["limit"] || 0;
+            let page = ((modelQuery.options["skip"] || 0) / limit) + 1;
+
+            if (limit === 0) return resolve({ data });
+
+            modelQuery.model.count(modelQuery._conditions, (err, total) => {
+                if (err) return reject(err);
+
+                const url = "http://foo.bar";
+                const startIndex = (page - 1) * limit + 1;
+                const endIndex = page * limit;
+                const total_pages = Math.ceil(total / limit) || 1;
+
+                let prev_page = null;
+                let next_page = null;
+                let links = {
+                    first: `${url}?page=1`,
+                    last: `${url}?page=${total_pages}`,
+                    prev: null,
+                    next: null,
+                };
+
+                if (page > 1) {
+                    prev_page = page - 1;
+                    links.prev = `${url}?page=${prev_page}`
+                }
+
+                if (endIndex < total) {
+                    next_page = page + 1;
+                    links.next = `${url}?page=${next_page}`
+                }
+
+                resolve({
+                    data,
+                    pagination: {
+                        total,
+                        per_page: limit,
+                        current_page: page,
+                        prev_page,
+                        next_page,
+                        total_pages,
+                        from: startIndex,
+                        to: endIndex,
+                        links,
+                    }
+                });
+            });
+        });
+    });
+};
