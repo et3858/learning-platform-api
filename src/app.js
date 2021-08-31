39e5c501c6cd44267f03ca7d88b408751561abab
Eduardo Tejada <et3858@gmail.com>
2021-08-31 17:28:00 -0400

    feat(app): formatea código con eslint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
-const dotenv = require('dotenv');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+const dotenv = require("dotenv");
 
 dotenv.config(); // Read the '.env' file
 
@@ -17,66 +17,66 @@ const database = require("./config/database");
  * Sources of help:
  * - https://www.coreycleary.me/escaping-relative-path-hell
  * - https://kenichishibata.medium.com/3-ways-to-fix-relative-paths-in-nodejs-require-ffc7f89bd7e1
- * @param   string path
- * @returns object [require]
+ * @param   {string} path
+ * @return {object} [require]
  */
 global.include = path => require(__dirname + "/" + path);
 
-var apiRouter = require("./routes/api");
-var webRouter = require("./routes/web");
+const apiRouter = require("./routes/api");
+const webRouter = require("./routes/web");
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join('./', 'public'),
-  dest: path.join('./', 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+    src: path.join("./", "public"),
+    dest: path.join("./", "public"),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
 }));
-app.use(express.static(path.join('./', 'public')));
+app.use(express.static(path.join("./", "public")));
 
 
 // Bootstrap 5 dependencies
-app.use('/css', express.static(path.join('./', 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join('./', 'node_modules/bootstrap/dist/js')));
-// app.use('/js', express.static(path.join('./', 'node_modules/jquery/dist')));
+app.use("/css", express.static(path.join("./", "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join("./", "node_modules/bootstrap/dist/js")));
+// app.use("/js", express.static(path.join("./", "node_modules/jquery/dist")));
 
 // Defining 'web' and 'api' routes
-app.use('/', webRouter);
-app.use('/api', apiRouter);
+app.use("/", webRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // Connecting to a database using MongoDB
 if (process.env.NODE_ENV !== "test") {
-  database.connect();
+    database.connect();
 }
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.format({
-    "text/plain": () => res.send(err.message),
-    "text/html": () => res.render("error"),
-    "application/json": () => res.json(err),
-    default: () => res.status(406).send("Not Acceptable") // log the request and respond with 406
-  });
+    // render the error page
+    res.status(err.status || 500);
+    res.format({
+        "text/plain": () => res.send(err.message),
+        "text/html": () => res.render("error"),
+        "application/json": () => res.json(err),
+        "default": () => res.status(406).send("Not Acceptable") // log the request and respond with 406
+    });
 });
 
 module.exports = app;
